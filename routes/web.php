c8a14d3c6cb9a0a4d9837bdf5ba6b99c45719f91
matsu8392834 <matsu8392834@gmail.com>
2024-10-09 17:19:32 +0900

    matsu1009

--- routes/web.php
+++ routes/web.php
@@ -32,21 +32,21 @@ Route::prefix('home')->group(function () {
 
 //管理者ユーザーのみ
 
-Route::group(['middleware' => ['auth', 'can:admin']], function () {
+Route::prefix('users')->group(function () {
 
-    Route::get('/users', [App\Http\Controllers\UserController::class, 'index']);
-    Route::get('/users/edit/{id}', [App\Http\Controllers\UserController::class, 'edit']);
-    Route::post('/users/update', [App\Http\Controllers\UserController::class, 'update']);
-    Route::get('/users/delete/{id}', [App\Http\Controllers\UserController::class, 'delete']);
+    Route::get('/', [App\Http\Controllers\UserController::class, 'index']);
+    Route::get('/edit/{id}', [App\Http\Controllers\UserController::class, 'edit']);
+    Route::post('/update', [App\Http\Controllers\UserController::class, 'update']);
+    Route::get('/delete/{id}', [App\Http\Controllers\UserController::class, 'delete']);
+});
 
-
-
-    Route::get('/items', [App\Http\Controllers\ItemController::class, 'index']);
-    Route::get('/items/add', [App\Http\Controllers\ItemController::class, 'add']);
-    Route::post('/items/add', [App\Http\Controllers\ItemController::class, 'add']);
-    Route::get('/items/edit/{id}', [App\Http\Controllers\ItemController::class, 'edit']);
-    Route::post('/items/update/{id}', [App\Http\Controllers\ItemController::class, 'update']);
-    Route::post('/items/destroy/{id}', [App\Http\Controllers\ItemController::class, 'destroy']);
-    Route::get('/items/list', [App\Http\Controllers\ItemController::class, 'find'])->name('list');
-    Route::get('/items/detail/{id}', [App\Http\Controllers\ItemController::class, 'detail']);
+    Route::prefix('items')->group(function () {
+    Route::get('/', [App\Http\Controllers\ItemController::class, 'index']);
+    Route::get('/add', [App\Http\Controllers\ItemController::class, 'add']);
+    Route::post('/add', [App\Http\Controllers\ItemController::class, 'add']);
+    Route::get('/edit/{id}', [App\Http\Controllers\ItemController::class, 'edit']);
+    Route::post('/update/{id}', [App\Http\Controllers\ItemController::class, 'update']);
+    Route::post('/destroy/{id}', [App\Http\Controllers\ItemController::class, 'destroy']);
+    Route::get('/list', [App\Http\Controllers\ItemController::class, 'find'])->name('list');
+    Route::get('/detail/{id}', [App\Http\Controllers\ItemController::class, 'detail']);
 });
